d804f6778f540f224343a5eb5a3a4251c8b6e0ab
shonmikl <mikhailegorov676@gmail.com>
2024-06-28 13:48:35 +0200

    comm

---
+++ person_desc.py
@@ -0,0 +1,17 @@
+# v = int(input("Enter a number: "))
+# b = float(input("Enter a number: "))
+#
+# c = b + v
+# print(c)
+
+a = int(1)
+b = float(1)
+print(a + b)
+
+def get(a, b):
+    add(a, b)
+    print(a + b)
+
+def add(a, b):
+    return a + b
+
